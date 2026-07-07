fd044ec4467f79f99485009791e6ee21ef8a3a3b
galaminow-cell <galaminow@gmail.com>
2026-07-07 18:51:11 +0300

    update bot for render

--- bot.py
+++ bot.py
@@ -1,14 +1,46 @@
 import asyncio
+import os
 
+from aiohttp import web
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
 from database import Database
-
 from app.handlers import router
 
 
+async def start_web_server():
+
+    async def handle(request):
+        return web.Response(
+            text="Bot is running"
+        )
+
+    app = web.Application()
+
+    app.router.add_get(
+        "/",
+        handle
+    )
+
+    runner = web.AppRunner(app)
+
+    await runner.setup()
+
+    site = web.TCPSite(
+        runner,
+        "0.0.0.0",
+        int(os.getenv("PORT", 10000))
+    )
+
+    await site.start()
+
+
+
 async def main():
+
+    await start_web_server()
+
 
     bot = Bot(
         token=BOT_TOKEN
@@ -22,6 +54,7 @@ async def main():
 
 
     db = Database()
+
     db.create_tables()
 
 
